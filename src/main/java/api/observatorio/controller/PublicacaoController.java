5c5230cdec5a05bdaa66ed935ebbd99a38c418da
Juawo <joaopedrojp2703@gmail.com>
2026-08-05 10:17:45 -0300

    adiciona configuração CORS para o controlador de categorias e publicações

--- src/main/java/api/observatorio/controller/PublicacaoController.java
+++ src/main/java/api/observatorio/controller/PublicacaoController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/publicacoes")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PublicacaoController {
 
     private final PublicacaoService service;
